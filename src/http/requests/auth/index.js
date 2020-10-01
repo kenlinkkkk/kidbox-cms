7ca887ecc6dbb5ddde3a652dacd76fe9ed4148f2
thanh <tungnt15ptit@gmail.com>
2020-10-01 17:03:15 +0700

    fix

--- src/http/requests/auth/index.js
+++ src/http/requests/auth/index.js
@@ -16,7 +16,6 @@ export default {
         },
         data
       }
-
     return axios(config)
   },
   getUserInfo(payload) {
--- src/store/auth/authState.js
+++ src/store/auth/authState.js
@@ -1,6 +1,5 @@
 const state = () => ({
   isAuthorization: null,
   userInfo: null,
-  token: null
 })
 export default state
