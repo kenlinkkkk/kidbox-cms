9bd5f79c8dada54913dd4e3099144da51e03b10b
thanh <tungnt15ptit@gmail.com>
2020-10-07 11:45:28 +0700

    change to baseURL

--- src/http/axios/index.js
+++ src/http/axios/index.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const baseURL = "https://kidbox.vn/"
-const localBase = "http://localhost:8001/"
+// const localBase = "http://localhost:8001/"
 
 export default axios.create({
-  baseURL: localBase
+  baseURL: baseURL
 })
