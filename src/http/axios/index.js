921cae8f514552b6eb7bfebba87d2d50fcee0c5e
thanh <tungnt15ptit@gmail.com>
2020-10-07 09:36:51 +0700

    fix login

--- src/http/axios/index.js
+++ src/http/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const baseURL = "https://kidbox.vn/"
-const localBase = "http://localhost:8000"
+const localBase = "http://localhost:8000/"
 
 export default axios.create({
   baseURL: localBase
